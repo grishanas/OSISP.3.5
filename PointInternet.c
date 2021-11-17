56e58375da72ea7793473c1280594a54f0ad1b07
grishanas <80056655+grishanas@users.noreply.github.com>
2021-11-17 12:41:14 +0300

    Update PointInternet.c

--- PointInternet.c
+++ PointInternet.c
@@ -13,9 +13,54 @@
 //
 
 
-
-DWORD WINAPI TCPListener(PTCPClient TCPConnection)
+DWORD WINAPI InternetSendMessage(PTCTConnect TCPConnection,char *DGram,int size)
 {
+	int size = sizeof(struct sockaddr_in);
+	if (sendto(TCPConnection->TCPSocket, DGram, size, 0, TCPConnection->TCPSockAdrr, size)==-1)
+	{
+		return -1;
+	}
+	return 1;
+	
+}
+
+
+DWORD WINAPI TCPListener(PTCTConnect TCPConnection)
+{
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+	{
+		return;
+	}
+
+	if ((TCPConnection->TCPSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_TCP)) == INVALID_SOCKET)
+	{
+		return;
+	}
+	struct sockaddr_in server;
+	server.sin_addr.s_addr = INADDR_ANY;
+	server.sin_port = htons(TCP_PORT);
+	server.sin_family = AF_INET;
+	if (bind(TCPConnection->TCPSocket, (struct sockaddr*)&server, sizeof(server)) == SOCKET_ERROR)
+	{
+		return;
+	}
+	char buffer[256];
+	int size = sizeof(server);
+	while (1)
+	{
+		memset(buffer, '\0', 256);
+		while (recvfrom(TCPConnection->TCPSocket, buffer, POINTINTERNET_BUFFERSIZE, 0, TCPConnection->TCPSockAdrr, &size) == -1);
+		switch (buffer[0])
+		{
+		case(1): 
+		{
+
+		}
+		default:
+			break;
+		}
+	}
 
 
 }
@@ -80,16 +125,16 @@ DWORD WINAPI UDPListenerFunction(PPointInternet Internet)
 		case(1): 
 			{
 			PTCTConnect TCPClient = HeapAlloc(Internet->heap, HEAP_ZERO_MEMORY, sizeof(TCPConnect));
-			TCPClient->TCPSocket = SocketInput;
-			TCPClient->TCPThread = CreateThread(NULL, 0, &TCPListener, TCPClient, 0, 0);
-			
+			TCPClient->TCPSockAdrr = SocketInput;
+			TCPClient->TCPThread = CreateThread(NULL, 0, &TCPListener, TCPClient, 0, NULL);
+
 
 			// нужно ли вообще заносить это сообщение. Лучше наверное сделать счетчик поток в структурке
 
-			PMessage list = HeapAlloc(Internet->heap, HEAP_ZERO_MEMORY, sizeof(struct _spis));
+			/*PMessage list = HeapAlloc(Internet->heap, HEAP_ZERO_MEMORY, sizeof(struct _spis));
 			list->buffer[0] = (char)SocketInput->sin_addr.S_un.S_addr;
 			list->Lenght = sizeof(SocketInput->sin_addr.S_un.S_addr);
-			Enqueue(Internet->Queue, list);
+			Enqueue(Internet->Queue, list);*/
 			}
 		default:
 			break;
@@ -108,6 +153,7 @@ PPointInternet CreateInternetConnection(HWND hwnd)
 	Internet->heap = heap;
 	Internet->hwnd = hwnd;
 	Internet->ParentThread = GetCurrentThread();
+	Internet->TCPList = CreateList();
 	PQueue Queue = CreateQueue();
 
 	Internet->UDPListenerThread = CreateThread(NULL, 0, &UDPListenerFunction, Internet, 0, NULL);
