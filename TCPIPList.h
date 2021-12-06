7f4aac3eadd2892af1e12b4b8618832e78dcaf1a
grishanas <80056655+grishanas@users.noreply.github.com>
2021-12-06 10:56:54 +0300

    Update TCPIPList.h

--- TCPIPList.h
+++ TCPIPList.h
@@ -19,7 +19,6 @@ typedef struct __List {
 	HANDLE Heap;
 } *PTCPList,_TCPList;
 
-<<<<<<< HEAD
 int AddNewIP(PTCPList, PTCPConnect);
 PTCPConnect RemoveIP(PTCPList Connect, PTCPConnect ConnectSock);
 PTCPList CreateList();
@@ -28,8 +27,8 @@ PTCPConnect GetIP(PTCPList List, int index);
 PTCPList GetUsers(PTCPList Source);
 int RemoveUsers(PTCPList);
 PTCPConnect GetUserOFIndex(PTCPList List, int index);
-=======
-int AddNewIP(PTCPList, PTCTConnect);
-PTCTConnect RemoveIP(PTCPList Connect, PTCTConnect ConnectSock);
+
+int AddNewIP(PTCPList, PTCPConnect);
+PTCPConnect RemoveIP(PTCPList Connect, PTCPConnect ConnectSock);
 PTCPList CreateList();
->>>>>>> f219004ec1c2ce2bfadf8ceba7a62d97c4c8e9b1
+
